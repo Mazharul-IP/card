51aed349a43a507842ce7aa9fcf500a00c7ec1cf
Mazharul-IP <mazharul.bd.net@gmail.com>
2023-04-30 20:39:26 +0530

    first commit

---
+++ card.js
@@ -0,0 +1,55 @@
+
+let  a=document.getElementById("btn1").addEventListener('click',function()
+{
+    alert("Are You interested to know About me")
+    window.location="resume.html";
+});
+
+
+let b=document.getElementById("btn2").addEventListener('click',function()
+{
+    alert("Are You interested Hire me")
+    window.location="cardform.html";
+});
+
+let  d=document.getElementById("btn3").addEventListener('click',function()
+{
+    alert("Are You interested to know About me")
+    window.location="resume.html";
+});
+let c=document.getElementById("btn4").addEventListener('click',function()
+{
+    alert("Are You interested Hire me")
+    window.location="cardform.html";
+});
+
+
+let  e=document.getElementById("btn5").addEventListener('click',function()
+{
+    alert("Are You interested to know About me")
+    window.location="resume.html";
+});
+
+let f=document.getElementById("btn6").addEventListener('click',function()
+{
+    alert("Are You interested Hire me")
+    window.location="cardform.html";
+});
+
+
+
+// var swiper=new swiper(".mySwiper",{
+//     slidesPerView:3,
+//     spaceBetween:30,
+//     slidesPerGroup:3,
+//     loop:true,
+//     loopFillGroupWithBlank:true,
+//     pagination:{
+//         el:".swiper-pagination",
+//         clickable:true,
+//     },
+//     navigation:{
+//         nextEl:".swiper-button-next",
+//         prevEl:".swiper-button-prev",
+//     },
+// });
